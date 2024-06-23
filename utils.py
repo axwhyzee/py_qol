9831e86f73585a7eb3567474fb5079430cf91ec1
Siah Wee Hung <34325457+axwhyzee@users.noreply.github.com>
2024-06-23 06:06:58 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,12 +4,27 @@ import logging
 import os
 import pickle
 import time
-from typing import Any, Callable, Optional
+from pathlib import Path
+from typing import Any, Callable, Optional, Iterator
+import re
 
 
 logger = logging.getLogger()
 
 CACHE_DIR = '.cache_disk'
+
+
+def crawl_dir(root_path: Path, file_regex: str | None = None) -> Iterator[Path]:
+    """Recursively search for files with optional regex starting froma root_path"""
+    for path in root_path.iterdir():
+        if path.is_dir():
+            yield from crawl_dir(path, file_regex)
+        else:
+            if not file_regex:
+                yield path
+            elif re.search(file_regex, str(path)):
+                yield path
+                
 
 def timer(
     func: Callable
